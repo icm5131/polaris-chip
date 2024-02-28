1a6cf7a016cf9a62af8ebbd1af7d131dcb227e4f
icm5131 <icm5131@psu.edu>
2024-02-27 20:04:32 -0500

    open/close alerts 02/27/24

--- src/alert-app.js
+++ src/alert-app.js
@@ -21,98 +21,119 @@ export class AlertApp extends LitElement {
 
   static get styles() {
     return css`
-          :host([notice]) .alert-message{
-            background-color: lightblue;
-          }
+      .alert-wrapper {
+        display: flex;
+        width: 100%;
+        margin: 0px;
+      }
 
-          :host([notice]) .toggle{
-            background-color: darkblue;
-          }
-          
-          :host([notice]) .date-time{
-            background-color: darkblue;
-          }
+      .date-time {
+        background-color: darkgoldenrod;
+        color: white;
+        padding: 8px;
+        width: 30%;
+        flex: 1;
+        margin: 0px;
+      }
 
-          :host([warning]) .alert-message{
-            background-color: gold;
-          }
+      .alert-message {
+        display: none;
+        background-color: gold;
+        padding: 8px;
+        width: 40%;
+        margin: 0px;
+      }
 
-          :host([warning]) .toggle{
-            background-color: darkgoldenrod;
-          }
-          
-          :host([warning]) .date-time{
-            background-color: darkgoldenrod;
-          }
+      .toggle {
+        color: white;
+        background-color: darkgoldenrod;
+        padding: 8px;
+        width: 30%;
+        margin: 0px;
+      }
 
-          :host([alert]) .alert-message{
-            background-color: lightcoral;
-          }
+      :host([notice]) .alert-message {
+        background-color: lightblue;
+      }
 
-          :host([alert]) .toggle{
-            background-color: darkred;
-          }
-          
-          :host([alert]) .date-time{
-            background-color: darkred;
-          }
+      :host([notice]) .toggle {
+        background-color: darkblue;
+      }
+      
+      :host([notice]) .date-time {
+        background-color: darkblue;
+      }
 
-          :host([])
+      :host([warning]) .alert-message {
+        background-color: gold;
+      }
 
-          .alert-wrapper {
-            display: flex;
-            width: 100%;
-            margin: 0px;
-          }
+      :host([warning]) .toggle {
+        background-color: darkgoldenrod;
+      }
+      
+      :host([warning]) .date-time {
+        background-color: darkgoldenrod;
+      }
 
-          .date-time {
-            background-color: darkgoldenrod;
-            color: white;
-            padding: 8px;
-            width: 10%;
-            flex: 1;
-            margin: 0px;
-          }
+      :host([alert]) .alert-message {
+        background-color: lightcoral;
+      }
 
-          .alert-message {
-            background-color: gold;
-            padding: 8px;
-            width: 80%;
-            margin: 0px;
-          }
+      :host([alert]) .toggle {
+        background-color: darkred;
+      }
+      
+      :host([alert]) .date-time {
+        background-color: darkred;
+      }
 
-          .toggle {
-            color: white;
-            background-color: darkgoldenrod;
-            padding: 8px;
-            width: 10%;
-            margin: 0px;
-          }
+      :host([open]) .alert-message {
+        display: flex;
+        width: 80%;
+      }
+
+      :host([open]) .date-time {
+        width: 10%;
+      }
+
+      :host([open]) .toggle {
+        width: 10%;
+      }
+
+      :host() .
     `;
   }
 
   toggleAlert() {
     this.open = !this.open;
+    console.log(this.open);
   }
 
   render() {
     return html`
           <div class="alert-wrapper">
             <div class="date-time">${this.date}</div>
-            <div class="alert-message">
+            <div class="alert-message" ?open="${this.open}">
               <slot>${this.alertMessage}</slot>
             </div>
+            <div class="alert-title">
+              
+            </div>
             </details>
-            <div class="toggle" ?open="${this.open}" @click="${this.toggleAlert}">X</div>
+            <div class="toggle" ?open="${this.open}" @click="${this.toggleAlert}">
+              X
+              <!-- <button id="toggleAlert" class="alertbtn"></button> -->
+            </div>
           </div>
     `;
   }
 
   static get properties() {
     return {
-      date: { type: String },
-      alertMessage: { type: String, attribute: "alert-message" },
-      open: { type: Boolean },
+      date: { type: String, reflect: true },
+      alertMessage: { type: String, attribute: "alert-message", reflect: true },
+      open: { type: Boolean, reflect: true },
     };
   }
 }
