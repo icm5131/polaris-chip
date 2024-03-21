8a22db0955a704331e91231cfaaeffc0b1931f0e
Ian Melius <icm5131@psu.edu>
2024-03-21 13:17:29 -0400

    adding characters by name 03/21

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -1,10 +1,18 @@
 import { html, css } from "lit";
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+import "@lrnwebcomponents/rpg-character/rpg-character.js";
 
 export class HaxcmsPartyUI extends DDD {
 
     static get tag() {
         return 'haxcms-party-ui';
+    }
+
+    constructor() {
+        super();
+
+        this.users = [];
+        this.userName = "Name";
     }
 
     static get styles() {
@@ -22,7 +30,7 @@ export class HaxcmsPartyUI extends DDD {
                     text-align: center;
                     border: var(--ddd-border-lg);
                     border-color: var(--ddd-theme-default-coalyGray);
-                    background-color: var(--ddd-theme-default-slateMaxLight);
+                    background-color: var(--ddd-theme-default-limestoneLight);
                 }
 
                 .user-button {
@@ -30,19 +38,51 @@ export class HaxcmsPartyUI extends DDD {
                     border-color: var(--ddd-theme-default-beaverBlue);
 
                 }
+
+                .user-card {
+                    display: inline-flex;
+                    flex-wrap: wrap;
+                }
             `
         ];
     }
+
+     updateName(event) {
+        this.userName = event.target.value;
+    }
+
+    addUser(e) {
+        const randomNumber = globalThis.crypto.getRandomValues(new Uint32Array(1))[0];
+        
+
+        const user = {
+          id: randomNumber,
+          title: this.userName,
+          content: "Some content of some kind",
+          coolness: 7
+        }
+        console.log(user);
+        // push by itself is not a mutating operation
+        this.users.push(user);
+        this.requestUpdate();
+        //this.items = [...this.items, item];
+        console.log(this.users);
+      }
 
     render() {
         return html`
             <div class="party-ui-wrapper">
                 <div class="input-wrapper">
-                    <input type="text" class="username-add">
-                    <button id="useradd-button" class="user-button b-md">Add User</button>
+                    <input type="text" class="username-add" id="username-input" @input="${this.updateName}"/>
+                    <button class="user-button b-md" @click="${this.addUser}">Add User</button>
                 </div>
                 <div class="users-panel">
-                    
+                    ${this.users.map((user) => html`
+                        <div class="user-card">
+                            <rpg-character seed="${user.title}"></rpg-character>
+                            X
+                        </div>
+                    `)}
                 </div>
             </div>
         `;
@@ -51,7 +91,8 @@ export class HaxcmsPartyUI extends DDD {
     static get properties() {
         return {
             ...super.properties,
-            title: { type: String }
+            users: { type: Array },
+            userName: { type: String, attribute: "user-name"}
         }
     }
 }
